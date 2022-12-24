66e84346297652e80c4f9b728acbd77edfb5dcd3
gilshmaya <t-gilshmay@microsoft.com>
2022-12-24 12:59:08 +0200

    add

---
+++ MainLogic/src/main/java/MainLogic.java
@@ -0,0 +1,9 @@
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import org.apache.log4j.BasicConfigurator;
+
+public class MainLogic {
+    public static void main(String[] args) {
+    }
+}
